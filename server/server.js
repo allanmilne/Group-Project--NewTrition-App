543b825347357fc5bdbf55a0876d8f0f5515b333
Allan Milne <amilne.wd@gmail.com>
2019-09-20 15:43:11 +0100

    pulling api into server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const cors = require('cors')
 const parser = require('body-parser');
 const app = express();
+const dotenv = require('dotenv');
 
+dotenv.config();
 app.use(cors());
 app.use(parser.json());
+
+// const apiKey = process.env.API_KEY;
+// fetch(url, {
+//   headers: {
+//     'X-API-KEY': apiKey
+//   }
+// })
+// .then(response => response.json())
+// .then(data => this.dogImgURL = data.message);
