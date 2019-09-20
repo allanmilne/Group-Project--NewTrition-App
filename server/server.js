599accf6e9f46fd20674e2b97828c36005b52549
hayden <haydenwb@gmail.com>
2019-09-20 16:56:39 +0100

    ports functioning, some work required to get fetch to pull through correct info

--- server/server.js
+++ server/server.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const cors = require('cors')
 const parser = require('body-parser');
-const app = express();
 const dotenv = require('dotenv');
+const fetch = require("node-fetch");
+
+const app = express();
+const apiKey = process.env.API_KEY;
+const apiID = process.env.API_ID;
 
 dotenv.config();
 app.use(cors());
 app.use(parser.json());
 
-// const apiKey = process.env.API_KEY;
-// fetch(url, {
-//   headers: {
-//     'X-API-KEY': apiKey
-//   }
-// })
-// .then(response => response.json())
-// .then(data => this.dogImgURL = data.message);
+const api = fetch('https://trackapi.nutritionix.com/v2/search/item?nix_item_id=513fc9e73fe3ffd40300109f', {
+  headers: {
+    'X-API-KEY': apiKey,
+    'X-APP-ID': apiID
+  }
+})
+.then(response => response.json())
+.then(data => this.dogImgURL = data.message);
+
+app.get('/', function (req, res) {
+  res.send("Hello world");
+})
+
+app.listen(3000, function () {
+  console.log("App running on port 3000");
+});
