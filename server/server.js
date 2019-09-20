343cc64e09f6dbfcb92ca54c046d07a2d0c4309a
hayden <haydenwb@gmail.com>
2019-09-21 00:59:47 +0100

    I think the key and fetch is now working, but still cannot see items.

--- server/server.js
+++ server/server.js
@@ -2,32 +2,32 @@ const express = require('express');
 const cors = require('cors')
 const parser = require('body-parser');
 const dotenv = require('dotenv');
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
 const app = express();
 const apiKey = process.env.API_KEY;
 const apiID = process.env.API_ID;
 
-const data = null
+let storedData = null
 
 dotenv.config();
 app.use(cors());
 app.use(parser.json());
 
-const api = fetch('https://trackapi.nutritionix.com/v2/search/item?nix_item_id=513fc9e73fe3ffd40300109f', {
+fetch('https://trackapi.nutritionix.com/v2/search/item?nix_item_id=513fc9e73fe3ffd40300109f', {
   headers: {
-    'X-API-KEY': apiKey,
-    'X-APP-ID': apiID,
-    'query': "Cookies `n Cream"
+    'x-app-key': apiKey,
+    'x-app-id': apiID
   }
 })
 .then(response => response.json())
-.then(data => this.data = data);
+.then(data => this.storedData = data)
+.then(console.log(this.storedData));
 
 app.get('/', function (req, res) {
-  res.send("Hello world");
+  res.send(`Hello!`);
 })
 
 app.listen(3000, function () {
-  console.log("App running on port 3000");
+  console.log('App running on port 3000');
 });
