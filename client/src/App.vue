b30d37e2218724093825895a9ebcffb1b1e7e83f
Amanda Williamson <aselliott4@aol.co.uk>
2019-09-21 08:30:10 +0100

    calorie counting methods added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,1 +1,16 @@
+<template lang="html">
+  <div id="app">
+    <calorie-counter/>
+  </div>
+</template>
 
+<script>
+import CalorieCounter from './components/CalorieCounter';
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ client/src/components/CalorieCounter.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+    methods: {
+        menBmr(weight, height, age, activityLevel) {
+            BMR = 88.362 + (13.39 * weight) + (4.799 * height) - (5.677 * age)
+            return total = BMR * activityLevel
+        },
+        womenBmr(weight, height, age, activityLevel) {
+            BMR = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
+            return total = BMR * activityLevel
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
