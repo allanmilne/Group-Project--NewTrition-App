5f01b54a68ebb55a2b7763e6a288bf591ecba613
Rob Armitage <robert.c.armitage@gmail.com>
2019-09-24 14:45:01 +0100

    faded background image

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,41 +1,50 @@
 <template lang="html">
-  <div id="app">
-    <div class="hello">
-      <h1>Nutrition App</h1>
-      <p>Find out how good your food is!</p>
-      <vue-tiny-tabs id="mytabs" :anchor="false" :closable="true" :hideTitle="false" @on-close="onClose" @on-before="onBefore" @on-after="onAfter">
+  <body>
+    <div id="app">
+      <div class="hello">
 
-          <div class="section" id="tab1-nutrition-search">
-            <h3 class="title">Tab 1: Nutrition Information</h3>
+        <div id="header">
+          <h1>Nutrition App</h1>
+          <p>Find out how good your food is!</p>
+        </div>
 
-            <div class="columns">
-              <div id="nutrition-info">
-                <p>Nutrition info here!!!!</p>
+        <div id="tabs-container">
+          <vue-tiny-tabs id="mytabs" :anchor="false" :closable="true" :hideTitle="false" @on-close="onClose" @on-before="onBefore" @on-after="onAfter">
+
+              <div class="section" id="tab1-nutrition-search">
+                <h3 class="title">Tab 1: Nutrition Information</h3>
+
+                <div class="columns">
+                  <div id="nutrition-info">
+                    <p>Nutrition info here!!!!</p>
+                  </div>
+
+                  <div id="food-search-info">
+                    <h4>[Search form goes here]</h4>
+                    <breakdown-chart/>
+                  </div>
+                </div>
+
               </div>
 
-              <div id="food-search-info">
-                <h4>[Search form goes here]</h4>
-                <breakdown-chart/>
+              <div class="section" id="tab2-calorie-counter">
+                <h3 class="title">Tab 2: Calorie Calculator</h3>
+                <calorie-counter/>
               </div>
-            </div>
+        		<!-- <div class="section" id="tab3-daily-total">
+        			<h3 class="title">Tab 3: Daily Total</h3>
+              <ul>
+                <li>Form for entering food</li>
+                <li>List of food entered for day</li>
+                <li>Graph showing "actual" and "recommended" calories</li>
+              </ul>
+        		</div> -->
+        	</vue-tiny-tabs>
+        </div>
 
-          </div>
-
-          <div class="section" id="tab2-calorie-counter">
-            <h3 class="title">Tab 2: Calorie Calculator</h3>
-            <calorie-counter/>
-          </div>
-  		<!-- <div class="section" id="tab3-daily-total">
-  			<h3 class="title">Tab 3: Daily Total</h3>
-        <ul>
-          <li>Form for entering food</li>
-          <li>List of food entered for day</li>
-          <li>Graph showing "actual" and "recommended" calories</li>
-        </ul>
-  		</div> -->
-  	</vue-tiny-tabs>
+      </div>
     </div>
-  </div>
+  </body>
 </template>
 
 <script>
@@ -67,6 +76,15 @@ export default {
 </script>
 
 <style>
+body {
+  background-image: url('../public/food-background-image-04.jpg');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  background-color: #ece3cb;
+}
+
 h3 {
   text-align: center;
 }
@@ -79,7 +97,17 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  /* color: #42b983; */
+}
+
+#header {
+  text-align: center;
+  line-height: 1.2em;
+  margin: 4% 0 2% 0;
+}
+
+#tabs-container {
+  margin: 1.5%;
 }
 
 .tinytabs .tabs {
@@ -120,7 +148,8 @@ a {
 
 #nutrition-info{
   width: 35%;
-  background-color: #ece3cb;
+  /* background-color: #ece3cb; */
+  background-color: #ede5d0;
   margin: 1%;
   padding: 1%;
   border-radius: 1%;
@@ -131,7 +160,8 @@ a {
 
 #food-search-info{
   width: 55%;
-  background-color: #ece3cb;
+  /* background-color: #ece3cb; */
+  background-color: #ede5d0;
   margin: 1%;
   padding: 1%;
   border-radius: 1%;
