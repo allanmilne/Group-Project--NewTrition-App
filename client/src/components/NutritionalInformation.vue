de2f10d4101a4c7a0605b52eea873f9c332f3bd8
Amanda Williamson <aselliott4@aol.co.uk>
2019-09-24 16:52:45 +0100

    Transition CSS added

--- client/src/components/NutritionalInformation.vue
+++ client/src/components/NutritionalInformation.vue
@@ -2,18 +2,28 @@
   <div id="info">
     <h4>Click on each box below for more information... </h4>
       <div id="label">
-        <p v-on:click="showCalories = !showCalories">Calories</p>    
-        <p v-on:click="showFats = !showFats">Fats <br> less than 70g</p>
-        <p v-on:click="showSatFats = !showSatFats">Saturated fats <br> less than 20g</p>
-        <p v-on:click="showSugars = !showSugars">Sugars <br> less than 30g</p>
-        <p v-on:click="showSalt = !showSalt">Salt <br> less than 6g</p>
+        <p v-on:click="showCalories = !showCalories">Cals</p>    
+        <p v-on:click="showFats = !showFats">Fats</p>
+        <p v-on:click="showSatFats = !showSatFats">Sat fat</p>
+        <p v-on:click="showSugars = !showSugars">Sugars</p>
+        <p v-on:click="showSalt = !showSalt">Salt</p>
       </div>
       <div id="nutrients">
-        <p v-if="showCalories">Calories: The amount of energy in a food or drink is measured in a calorie. When we eat and drink more calories than we use up, our bodies store the excess as body fat. Use our Calorie Calculator to measure how many calories you should aim for per day!</p>
-        <p v-if="showFats">Fats: A small amount of fat is an essential part of a healthy, balanced diet. Fat is a source of essential fatty acids, which the body cannot make itself.  Fat helps the body absorb vitamin’s A, D and E/ These vitamins are fat-soluble, meaning they can only be absorbed with the help of fats. Any fat not used by your body's cells or to create energy is converted into body fat.</p>
-        <p v-if="showSatFats">Saturated fats: Most people in the UK eat too much saturated fats. Eating lots of saturated fat can raise your cholesterol and increase your risk of heart disease.</p>
-        <p v-if=showSugars>Sugars: Eating too much sugar can make you gain weight and can lead to tooth decay. The government recommend that Sugars should not make up more than 5% of the energy you from food and drink each day.</p>
-        <p v-if="showSalt">Salt: A diet high in salt can cause raised blood pressure, which can increase your risk of heart disease and stroke. High blood pressure often has no symptoms, and many people who have high blood pressure don’t know it.</p>
+        <transition name="bounce">
+          <p v-if="showCalories">Calories: The amount of energy in a food or drink is measured in a calorie. When we eat and drink more calories than we use up, our bodies store the excess as body fat. Use our Calorie Calculator to measure how many calories you should aim for per day!</p>
+        </transition>
+        <transition name="bounce">
+          <p v-if="showFats">Fats: A small amount of fat is an essential part of a healthy, balanced diet. Fat is a source of essential fatty acids, which the body cannot make itself.  Fat helps the body absorb vitamin’s A, D and E/ These vitamins are fat-soluble, meaning they can only be absorbed with the help of fats. Any fat not used by your body's cells or to create energy is converted into body fat.</p>
+        </transition>
+        <transition name="bounce">
+          <p v-if="showSatFats">Saturated fats: Most people in the UK eat too much saturated fats. Eating lots of saturated fat can raise your cholesterol and increase your risk of heart disease.</p>
+        </transition>
+        <transition name="bounce">
+          <p v-if=showSugars>Sugars: Eating too much sugar can make you gain weight and can lead to tooth decay. The government recommend that Sugars should not make up more than 5% of the energy you from food and drink each day.</p>
+        </transition>
+        <transition name="bounce">
+          <p v-if="showSalt">Salt: A diet high in salt can cause raised blood pressure, which can increase your risk of heart disease and stroke. High blood pressure often has no symptoms, and many people who have high blood pressure don’t know it.</p>
+        </transition>
       </div>
   </div>
 </template>
@@ -51,12 +61,12 @@ export default {
     
     #label p{
         border: 1px solid white;
-        border-radius: 10px;
+        border-radius: 15px;
         width: 13%;
-        height: 80px;
+        height: 90px;
         background-color: green;
         display: inline-block;
-        flex-direction: column;
+        justify-content: center;
         padding: 4px 10px;
         text-align: center;
     }
@@ -64,4 +74,25 @@ export default {
     h4 {
       text-align: center;
     }
+
+    .bounce-enter-active {
+      animation: bounce-in .5s;
+    }
+
+    .bounce-leave-active {
+      animation: bounce-in .5s reverse;
+    }
+
+    @keyframes bounce-in {
+      0% {
+        transform: scale(0);
+      }
+      50% {
+        transform: scale(1.5);
+      }
+      100% {
+        transform: scale(1);
+      }
+    }
+
 </style>
